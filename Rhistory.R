3333ea6702aa0f5ca34203bbd9b6ab8f148a479e
Noha M. Abdelhady <44941960+NOHAHADY@users.noreply.github.com>
2019-01-12 17:59:14 +0200

    Add files via upload

---
+++ Rhistory.R
@@ -0,0 +1,460 @@
+getwd()
+q()
+local({fn<-choose.files(filters=Filters[c('R','txt','All'),],index=4)
+file.show(fn,header=fn,title='')})
+find.package("devtools")
+install.package("devtools")
+install.packages("devtools")
+library(devtools)
+find_rtools()
+library(pkgbuild)
+install.packages("devtools")
+find.package("devtools")
+library(devtools)
+find_rtools()
+find.package("devtools")
+library(devtools)
+find_rtools()
+find.package("devtools")
+library(devtools)
+find_rtools()
+Sys.getenv('PATH')
+library(devtools)
+find_rtools()
+q()
+getwd()
+dir()
+q()
+install.packages("swirl")
+install.packages("swirl")
+packageVersion("swirl")
+install.packages("swirl")
+packageVersion("swirl")
+library(swirl)
+install_from_swirl("R Programming")
+swirl()
+install.packages("swirl")
+install.packages("swirl")
+packageVersion("swirl")
+library(swirl)
+install.packages("stringi")
+install.packages("stringi", type = "source")
+install.packages("swirl")
+packageVersion("swirl")
+library(swirl)
+install_from_swirl("R Programming")
+swirl()
+5+7
+x<-5+7
+x
+y<-x-3
+y
+z<-c(1.1,99,3.14)
+z<-c( 1.1, 9, 3.14)
+?c
+z
+c(z,555,z)
+z*2 + 100
+my_sqrt<-sqrt(z-1)
+my_sqrt
+my_div<-z/my_sqrt
+my_div
+c(1, 2, 3, 4)
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+z*2 + 100
+z*2 + 1000
+my_div
+exit()
+bye()
+q()
+licence()
+demo()
+install.packages('rvest')
+install.packages('rvest')
+install.packages('rvest')
+pollutantmean <- function(directory,pollutant,id = 1:332){}
+pollutantmean <- function(directory,pollutant,id = 1:332){
+}
+pollutantmean <- function(directory,pollutant,id = 1:332){
+}
+MyData <- read.csv(file="C:\Users\Omar SamadOo0\Desktop\DATA SCIENCE COURSE\specdata\008.csv", header=TRUE, sep=",")
+MyData <- read.csv(file="C:\008.csv", header=TRUE, sep=",")
+getwd
+getwd()
+setwd(C:\Users\Omar SamadOo0\Desktop\DATA SCIENCE COURSE\specdata)
+setwd("\Desktop\DATA SCIENCE COURSE\specdata")
+setwd("Desktop\DATA SCIENCE COURSE\specdata")
+setwd("Desktop/DATA SCIENCE COURSE/specdata")
+getwd()
+setwd("Users")
+setwd("...\Users")
+setwd("~\Users")
+setwd("~/Users")
+dir <-("C:\Users\Omar SamadOo0\Desktop\DATA SCIENCE COURSE\specdata")
+dir <-("Users\Omar SamadOo0\Desktop\DATA SCIENCE COURSE\specdata")
+dir <-("\Users\Omar SamadOo0\Desktop\DATA SCIENCE COURSE\specdata")
+library(datasets)
+data(iris)
+?iris
+Sepal.Length
+iris$Sepal.Length
+apply(iris,1,mean)
+lapply(iris,1,mean)
+iris$Sepal.Length
+mean(iris$Sepal.Length)
+tapply(iris$Sepal.Length,virginica,mean)
+sapply(split(iris$Sepal.Length, iris$Species), mean)
+round(6.588 )
+rowMeans(iris[, 1:4])
+apply(iris[, 1:4], 1, mean)
+apply(iris[, 1:4], 2, mean)
+library(datasets)
+data(mtcars)
+?mtcars
+mean(mtcars$mpg, mtcars$cyl)
+sapply(split(mtcars$mpg, mtcars$cyl), mean)
+split(mtcars, mtcars$cyl)
+sapply(split(mtcars$mpg, mtcars$cyl), mean)
+sapply(split(mtcars$hp, mtcars$cyl), mean)
+209.21429-82.63636
+round(126.5779)
+debug(ls)
+ls
+library("swirl")
+swirl()
+x
+x[1:10]
+x[is.na(x)]
+y<-x[!is.na(x)]
+y
+y[y>0]
+x[x>0]
+x[!is.na(x)x>0]
+x[!is.na(x)&x>0]
+x[3]
+x[c(3,5,7)]
+x[0]
+x[3000]
+x[c(-2,-10)]
+x[-c(2,10)]
+vect<-c(foo=11,bar==2,norf=NA)
+vect<-c(foo=11,bar=2,norf=NA)
+vect
+names(vect)
+vect2<-c(11,2,NA)
+names(vect<-c("foo","bar","norf"))
+names(vect)<-c("foo","bar","norf")
+names(vect2)<-c("foo","bar","norf")
+identical(vect,vect2)
+vect["bar"]
+vect[c("foo","bar")]
+swirl()
+my_vector<-c(1:20)
+my_vector<-1:20
+my_vector
+dim(my_vector)
+length(my_vector)
+dim(my_vector)<-c(4,5)
+dim(my_vector)
+attributes(my_vector)
+my_vector
+class(my_vector)
+my_matrix<-my_vector
+?matrix()
+?matrix
+my_matrix2<-matrix(1:20,nrow = 4,ncol = 5)
+identical(my_matrix,my_matrix2)
+patients<-c("Bill","Gina","Kelly","Sean")
+cbind(patients,my_matrix)
+my_data<-data.frame(patients,my_matrix)
+my_data
+class(my_data)
+cnames<-c("patient", "age", "weight", "bp", "rating", "test")
+colnames(colnames("cnames"))
+colnames(colnames(my_data))<-cnames
+colnames(my_data)<-cnames
+my_data
+bye()
+swirl()
+bye()
+swirl()
+Sys.Date()
+mean(C(2,4,5))
+mean(C(2,4,5))
+mean(C(2, 4, 5))
+mean(C(2, 4, 5))
+mean(C(2, 4, 5))
+mean(C(2, 4, 5))
+mean(C(2, 4, 5))
+mean(C(2, 4, 5))
+mean(C(2, 4, 5))
+mean(C(2, 4, 5))
+hmmfm
+mean(c(2,4,5))
+submit
+submit()
+boring_function('My first function!')
+library("swirl")
+swirl()
+boring_function
+submit()
+my_mean(c(4,5,10))
+submit()
+submit()
+submit()
+submit()
+remainder(5)
+remainder(11,5)
+remainder(divisor=11,num=5)
+remainder(4,div=2)
+args(remainder)
+submit()
+evaluate(diviation,c(1.4, 3.6, 7.9, 8.8))
+evaluate(sd,c(1.4, 3.6, 7.9, 8.8))
+evaluate(function(x){x+1}, 6)
+evaluate(function(x){x[1]}, c(8, 4, 0))
+evaluate(function(x){x[3]}, c(8, 4, 0))
+evaluate(function(x){x[length(x)]}, c(8, 4, 0))
+?paste
+paste("programming","is","fun")
+paste("programming","is","fun!")
+paste("programming", "is", "fun!")
+paste("Programming", "is", "fun!")
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+?paste()
+paste
+submit()
+submit()
+submit()
+telegram("kosom ","swirl")
+submit()
+submit()
+submit()
+submit()
+mad_libs("x","y","z")
+submit()
+'I'%p% 'love'%p%'R!'
+head(flags)
+dim(flags)
+class(flags)
+Cls_list<-lapply(flags,class)
+Cls_list <- lapply(flags, class)
+cls_list <- lapply(flags, class)
+cls_list
+class(cls_list)
+as.character(cls_list)
+cls_vect<-sapply(flags, class)
+class(cls_vect)
+sum(flags$orange)
+flag_colors<-flags[,11:17]
+head(flag_colors)
+lapply(flag_colors,sum)
+sapply(flag_colors,sum)
+sapply(flag_colors,mean)
+flag_shapes<-flags[,19:23]
+lapply(flag_shapes, range)
+shape_mat<-sapply(flag_shapes,range)
+shape_mat
+class(shape_mat)
+unique(c(3, 4, 5, 5, 5, 6, 6))
+uniqe_vals<-lapply(flags,unique)
+unique_vals<-lapply(flags,unique)
+unique_vals
+sapply(unique_vals,length())
+sapply(unique_vals,length
+sapply(unique_vals,length)
+sapply(unique_vals,length)
+sapply(flags,unique)
+lapply(unique_vals, function(elem) elem[2])
+sapply(flags,unique)
+vapply(flags,unique,numeric(1))
+ok()
+sapply(flags,class)
+vapply(flags,class,character(1))
+?tapply
+table(flags$landmass)
+table(flags$animate)
+tapply(flags$animate,flags$landmass,mean)
+tapply(flags$population,flags$red,summary)
+tapply(flags$population,flags$landmass,summary)
+library(swirl)
+swirl()
+library(dyplr)
+library(dplyr)
+cran<-tbl_df(mydf)
+rm("mydf")
+cran
+?group_by
+by_package<-group_by(cran,package=variable)
+by_package<-group_by(cran)
+by_package<-group_by(cran,package)
+by_package
+summarize(by_package,mean(size))
+? n
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+summarize()
+?summarize()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+submit()
+?n
+submit()
+bye()
+library(swirl)
+swirl()
+Sys.getlocale("LC_TIME")
+setwd("C:/Users/Omar SamadOo0/Desktop/DATA SCIENCE COURSE/course_4")
+library("data.table")
+setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
+#Reads in data from file then subsets data for specified dates
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+, na.strings="?"
+)
+# Prevents histogram from printing in scientific notation
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+# Change Date Column to Date Type
+powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+# Filter Dates for 2007-02-01 and 2007-02-02
+powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+png("plot1.png", width=480, height=480)
+## Plot 1
+hist(powerDT[, Global_active_power], main="Global Active Power",
+xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.off()
+source(plot1.R)
+source(plot1)
+setwd("C:/Users/Omar SamadOo0/Desktop/DATA SCIENCE COURSE/course_4")
+source(plot1)
+source(plot1.R)
+library("data.table")
+setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
+#Reads in data from file then subsets data for specified dates
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+, na.strings="?"
+)
+# Prevents histogram from printing in scientific notation
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+# Change Date Column to Date Type
+powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+# Filter Dates for 2007-02-01 and 2007-02-02
+powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+png("plot1.png", width=480, height=480)
+## Plot 1
+hist(powerDT[, Global_active_power], main="Global Active Power",
+xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.off()
+setwd("C:/Users/Omar SamadOo0/Desktop/DATA SCIENCE COURSE/course_4")
+library("data.table")
+#Reads in data from file then subsets data for specified dates
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+, na.strings="?"
+)
+# Prevents histogram from printing in scientific notation
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+# Change Date Column to Date Type
+powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+# Filter Dates for 2007-02-01 and 2007-02-02
+powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+png("plot1.png", width=480, height=480)
+## Plot 1
+hist(powerDT[, Global_active_power], main="Global Active Power",
+xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.off()
+ls
+ls()
+source(histo.R)
+source('histo.R')
+library("data.table")
+setwd("C:/Users/Omar SamadOo0/Desktop/DATA SCIENCE COURSE/course_4")
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+, na.strings="?"
+)
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+png("plot1.png", width=480, height=480)
+hist(powerDT[, Global_active_power], main="Global Active Power",
+xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.off()
+library("data.table")
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+, na.strings="?"
+)
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+png("Notation.png", width=480, height=480)
+plot(x = powerDT[, dateTime]
+, y = powerDT[, Global_active_power]
+, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+, na.strings="?"
+)
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+png("notation2.png", width=480, height=480)
+plot(powerDT[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(powerDT[, dateTime], powerDT[, Sub_metering_2],col="red")
+lines(powerDT[, dateTime], powerDT[, Sub_metering_3],col="blue")
+legend("topright"
+, col=c("black","red","blue")
+, c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
+,lty=c(1,1), lwd=c(1,1))
+dev.off()
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+, na.strings="?"
+)
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+plot(powerDT[, dateTime], powerDT[, Global_active_power], type="l", xlab="", ylab="Global Active Power")
+plot(powerDT[, dateTime],powerDT[, Voltage], type="l", xlab="datetime", ylab="Voltage")
+plot(powerDT[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(powerDT[, dateTime], powerDT[, Sub_metering_2], col="red")
+lines(powerDT[, dateTime], powerDT[, Sub_metering_3],col="blue")
+legend("topright", col=c("black","red","blue")
+, c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
+, lty=c(1,1)
+, bty="n"
+, cex=.5)
+plot(powerDT[, dateTime], powerDT[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
+dev.off()
